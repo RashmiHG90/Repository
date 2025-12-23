1e71211edfd1721d9246e5aa8d928e147d9c6315
RashmiHG90 <rashmi.hguk@gmail.com>
2025-12-23 09:14:33 +0000

    added Math operations and Date operations js file

---
+++ JavaScript ES5 and ES6/DateOperation.js
@@ -0,0 +1,11 @@
+let dd= new Date();
+console.log("Current date and time: " + dd.toString());
+console.log(dd.getDate()); //get date
+console.log(dd.getMonth()); //get month, month index starts from 0
+console.log(dd.getMonth()+1); //get month, month index starts from 0
+console.log(dd.getFullYear());  //get year
+console.log(dd.getHours()); //get hours
+console.log(dd.getMinutes()); //get minutes
+console.log(dd.getSeconds()); //get seconds
+console.log(dd.getMilliseconds()); //get milliseconds
+console.log(dd.getDay()); //get day index, Sunday=0, Monday=1       
---
+++ JavaScript ES5 and ES6/MathOperations.js
@@ -0,0 +1,10 @@
+console.log(Math.PI)
+
+console.log(Math.sqrt(16))
+
+console.log(Math.pow(2,3))  
+console.log(Math.abs(-10))
+
+console.log(Math.ceil(4.2))  
+console.log(Math.floor(4.7))  
+console.log(Math.round(4.5))
